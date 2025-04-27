8f4bf9795dc5a7e841537b528e84139902627d44
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-27 16:07:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	const formObj = document.getElementById('form1')
+	const firstName = formObj.elements['fname'].value
+	const lastName = formObj.elements['lname'].value
+	alert(`${firstName} ${lastName}`)
 }
